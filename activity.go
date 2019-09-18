891c53a84e3cea6ea7c8b45b3c1f6361e0a316c7
banananajun <55434534+banananajun@users.noreply.github.com>
2019-09-18 09:07:12 +0800

    Update activity.go

--- activity.go
+++ activity.go
@@ -22,3 +22,51 @@ const (
 	PIN_MODE      = 5
 
 )
+
+
+type GrovePi struct {
+	i2cmodule hwio.I2CModule
+	i2cDevice hwio.I2CDevice
+	
+}
+
+
+//activity is Activity implementation
+type grovePiDWActivity struct {
+	sync.Mutex	
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new Activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &grovePiDWActvitiy{metadata: metadata}	
+}
+
+// Metadata implements.Activity.Metadata
+func (a *grovePiDWActivity) Eval(context activity.Context) (done bool, err error) {
+	
+	
+	var pin byte
+	var value bool
+	
+	log.Debug("Starting Pin Write")
+	
+	// Reading if the input is empty or not empty
+	if context.GetInput(ivPin) != nil {
+		pin = byte(context.GetInput(ivPin).(int))
+	}
+	if context.GetInput(ivValue) != nil {
+		value = context.GetInput(ivValue).(bool)
+	}
+	
+	var g *GrovePi
+	g = InitGrovePi(0x04)
+	err = g.PinMode(pin, "input")
+	if err != nil {
+		log.Error("GrovePi :: Set PinMode Error", err)
+	}
+	
+	//read the GrovePi
+	
+	
+	
